1c2e9b8251d6153582d35a4992d25f050ae8981b
hswerts <herculano@premiumbravo.com.br>
2025-10-16 22:30:16 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  // Remove o turbopack do build
-}
+// next.config.ts
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'premiumbravo-forecast'
 
-module.exports = nextConfig
+
+const nextConfig = {
+output: 'export', // gera site estático em /out
+basePath: isProd ? `/${repo}` : '',
+assetPrefix: isProd ? `/${repo}/` : '',
+images: { unoptimized: true }, // GitHub Pages não lida com Image Optimization
+trailingSlash: true, // ajuda no Pages (URLs com /)
+}
+export default nextConfig
