1992adf930422167cf38bba1a8d6a21561b08218
hswerts <herculano@premiumbravo.com.br>
2025-10-18 21:14:48 -0300

    mudança topbar

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { UserBadge } from '@/components/UserBadge'
+import { UserBadge } from './UserBadge'
 
 export default function TopBar() {
   const pathname = usePathname()
