47b19067544a43da3e08b8421e247a525374c6fa
hswerts <herculano@premiumbravo.com.br>
2025-10-23 19:42:22 -0300

    Update layout.tsx

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-teal-700">
+    <div className="min-h-screen flex items-center justify-center bg-premiumbravo">
       {children}
     </div>
   )
