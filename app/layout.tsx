fe1cffe362be6a726a45cb052f3bd5cf1f478c97
hswerts <herculano@premiumbravo.com.br>
2025-10-22 21:16:46 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,8 +17,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        {/* Layout raiz limpo - sem navbar */}
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   )
